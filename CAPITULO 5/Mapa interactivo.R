4e19898e23705ad3d5db4c4d656d92d97487fabc
Gonzalo Bravo <gonzalobravoargentina@gmail.com>
2024-03-14 08:50:07 -0300

    add zip

---
+++ CAPITULO 5/Mapa interactivo.R
@@ -0,0 +1,6 @@
+
+
+#install.packages(c("shiny", "shinythemes", "leaflet", "dplyr", "rinat", "devtools", "htmlwidgets", "openxlsx"))
+#devtools::install_github("rstudio/fontawesome")
+library(shiny)
+runGitHub("iNaturalist_app", "Vojczech") 
